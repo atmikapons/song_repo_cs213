99b390fa0aca440cbc4ef364afb8da88cfa2aa07
atmikapons <atmikapons@gmail.com>
2019-09-28 12:27:09 -0400

    added gui features

--- song_repo/src/view/Controller.java
+++ song_repo/src/view/Controller.java
@@ -1,5 +1,85 @@
 package view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
 public class Controller {
 
+	@FXML         
+	ListView<String> listView;                
+
+	private ObservableList<String> obsList;              
+
+	public void start(Stage mainStage) {                
+		// create an ObservableList 
+		// may want to populate obsList in a separate method
+		// so that obsList can be created from a text file when needed
+		
+		obsList = FXCollections.observableArrayList(                               
+				"Giants",                               
+				"Patriots",
+				"49ers",
+				"Rams",
+				"Packers",
+				"Colts",
+				"Cowboys",
+				"Broncos",
+				"Vikings",
+				"Dolphins",
+				"Titans",
+				"Seahawks",
+				"Steelers",
+				"Jaguars"); 
+
+		listView.setItems(obsList); 
+		
+		// select the first item
+	      listView.getSelectionModel().select(0);
+
+	      // set listener for the items
+	      listView
+	        .getSelectionModel()
+	        .selectedIndexProperty()
+	        .addListener(
+	           (obs, oldVal, newVal) -> 
+	               showItemInputDialog(mainStage));
+
+	}
+	
+//	private void showItem(Stage mainStage) {                
+//	      Alert alert = 
+//	         new Alert(AlertType.INFORMATION);
+//	      //alert.initModality(Modality.NONE);
+//	      alert.initOwner(mainStage);
+//	      alert.setTitle("List Item");
+//	      alert.setHeaderText(
+//	           "Selected list item properties");
+//
+//	      String content = "Index: " + 
+//	          listView.getSelectionModel()
+//	                   .getSelectedIndex() + 
+//	          "\nValue: " + 
+//	          listView.getSelectionModel()
+//	                   .getSelectedItem();
+//
+//	          alert.setContentText(content);
+//	          alert.showAndWait();
+//	          // alert.show();  
+//	          //System.out.println("hey there!");
+//	          
+//	   }
+	
+	private void showItemInputDialog(Stage mainStage) {                
+	      //String item = listView.getSelectionModel().getSelectedItem();
+	      //int index = listView.getSelectionModel().getSelectedIndex();
+
+	      //set fields in EditText Boxes equal to fields of selected song
+	   }
+
+
 }
+
+
